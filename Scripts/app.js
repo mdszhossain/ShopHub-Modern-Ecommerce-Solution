0e7d8f4e13a0a4eda4715e88f6c346fbcbf07df9
mdszhossain <personalmailshazzad@gmail.com>
2026-05-21 20:09:10 +0600

    Feature: Featured section created with responsiveness for multiple screen and Card's information fetched from fake store API

--- Scripts/app.js
+++ Scripts/app.js
@@ -170,3 +170,99 @@ categorySection.innerHTML = `
 `;
 
 main.appendChild(categorySection);
+
+// Featured Product Section Creation
+let featuredProduct = document.createElement("section");
+featuredProduct.classList.add(
+    "featured-product",
+    "relative",
+    "top-40",
+    "sm:p-8",
+    "md:px-20",
+    "bg-white",
+);
+featuredProduct.innerHTML = `
+    <div class="featured-text-container flex items-center justify-between pt-5">
+        <div class="featured-text">
+            <h2 class="featured-text-title font-bold text-2xl md:text-3xl text-center sm:text-left mb-3">Featured Products</h2>
+            <p class="featured-text-desc text-center mb-5">Check out our most popular items</p>
+        </div>
+        <button class="view-all-btn bg-[#155DFB] text-white px-5 py-2 rounded-xl">View All <i class="fa-solid fa-arrow-right"></i></button>
+    </div>
+`;
+
+main.appendChild(featuredProduct);
+
+// card information fetching
+
+async function featuredCardInfoFetch() {
+    try {
+        let res = await axios.get("https://fakestoreapi.com/products?limit=8");
+        return res.data;
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// Featured Cards Container creation
+let featuredCards = document.createElement("div");
+featuredCards.classList.add(
+    "featured-cards",
+    "sm:w-full",
+    "grid",
+    "gap-10",
+    "grid-cols-1",
+    "sm:grid-cols-2",
+    "lg:grid-cols-3",
+    "xl:grid-cols-4",
+    "mt-15"
+);
+featuredProduct.appendChild(featuredCards);
+
+featuredCardInfoFetch().then((data) => {
+    let products = data;
+    console.log(products);
+    for (product of products) {
+        let card = document.createElement("div");
+        card.classList.add(
+            "card",
+            "overflow-hidden",
+            "bg-[#F7F9FC]",
+            "rounded-3xl",
+            "shadow-[0_2px_12px_rgba(0,0,0,0.3)]",
+            "w-full"
+        );
+        card.innerHTML = `
+            <div class="card-img relative h-80 bg-[#F1F3F5] rounded-t-3xl flex items-center justify-center p-6">
+                <img 
+                    src="${product.image}" 
+                    alt="Fjallraven - Foldsack No. 1 Backpack" 
+                    class="h-full w-full object-contain"
+                />
+                
+                <button class="absolute top-4 right-4 bg-white p-3 rounded-full shadow-md text-gray-400 hover:text-red-500 transition-colors">
+                    <i class="fa-regular fa-heart"></i>
+                </button>
+            </div>
+
+            <div class="card-info-box p-5 bg-white rounded-b-3xl h-full">
+                <div class="rate-and-stock text-sm flex items-center gap-1">
+                    <i class="fa-solid fa-star text-amber-400 text-gold"></i>
+                    <span class="rating font-medium text-gray-900">${product.rating.rate}</span>
+                    <span class="stock text-gray-400">(<span class="stock-count">${product.rating.count}</span>)</span>
+                </div>
+                
+                <h3 class="card-title text-lg font-medium mt-3 text-gray-900 line-clamp-1">${product.title}</h3>
+                <p class="text-[#6A778E] text-sm mt-1 uppercase">${product.category}</p>
+                
+                <div class="flex justify-between items-center mt-5">
+                    <span class="text-2xl font-bold text-gray-900">$<span class="prod-price">${product.price}</span></span>
+                    <button class="add-cart-btn px-5 py-2.5 bg-[#155DFB] hover:bg-[#0c4ad4] text-white font-medium rounded-xl text-sm flex items-center gap-2 transition-colors">
+                        <i class="fa-solid fa-cart-shopping"></i> Add
+                    </button>
+                </div>
+            </div>
+        `;
+        featuredCards.appendChild(card);
+    }
+});
